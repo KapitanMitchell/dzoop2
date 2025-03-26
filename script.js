9aa67228906800ac0a87ff9796134366e2bf4caf
MAVERICK <agentyarikt7@gmail.com>
2025-03-26 11:02:25 +0200

    nre

---
+++ script.js
@@ -0,0 +1,29 @@
+class Account {
+    constructor({ login, email }) {
+      this.login = login;
+      this.email = email;
+    }
+  
+    getInfo() {
+      console.log(`Login: ${this.login}, Email: ${this.email}`);
+    }
+  }
+  
+  console.log(Account.prototype.getInfo);
+  
+  const mango = new Account({
+    login: 'Mangozedog',
+    email: 'mango@dog.woof',
+  });
+  
+  mango.getInfo(); 
+  
+  const poly = new Account({
+    login: 'Poly',
+    email: 'poly@mail.com',
+  });
+  
+  poly.getInfo();
+
+
+  
